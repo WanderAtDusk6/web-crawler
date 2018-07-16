69bd7735c81f913d88a4475d4d1b368843c4b550
WanderAtDusk6 <34180166+WanderAtDusk6@users.noreply.github.com>
2018-07-16 14:11:30 +0800

    Add files via upload

---
+++ scrapy框架练习3/BaiduStocks/BaiduStocks/spiders/stocks.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+import random
+
+#CSS Selector的基本使用
+#<HTML>.css('a::attr(href)').extract()
+#       标签名称     标签属性
+
+
+class StocksSpider(scrapy.Spider):
+    name = 'stocks'
+    start_urls = ['http://quote.eastmoney.com/stocklist.html']
+
+    user_agent_list = [\
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1"\
+            "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",\
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",\
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",\
+            "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",\
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",\
+            "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",\
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",\
+            "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",\
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",\
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",\
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",\
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",\
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",\
+            "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",\
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",\
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",\
+            "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+            ]
+
+    ua = random.choice(user_agent_list)#随机抽取User-Agent
+    headers = {
+              'Accept-Encoding':'gzip, deflate, sdch, br',
+              'Accept-Language':'zh-CN,zh;q=0.8',
+              'Connection':'keep-alive',
+              'Referer':'https://gupiao.baidu.com/',
+              'User-Agent':ua
+              }#构造请求头
+
+    # yield scrapy.Request(url,callback=self.parse,headers=headers)'''
+    def parse(self, response):
+        for href in response.css('a::attr(href)').extract(): # 据弹幕说返回的是字符串
+            try:
+                stock = re.findall(r"[s][hz]\d(6)",href)[0]
+                url = 'https://gupiao.baidu.com/stock/' + stock + '.html'
+                yield scrapy.Request(url, callback=self.parse_stock, headers=headers)
+                # callback=后面的是什么鬼？？？
+            except:
+                continue
+        pass
+
+    def parse_stock(self, response):
+        infoDict = {}
+        stockInfo = response.css('.stock-bets')
+        name = stockInfo.css('.bets-name').extract()[0]
+        keyList = stockInfo.css('dt').extract()    # dt 与dd标签
+        valueList = stockInfo.css('dd').extract()
+        for i in range(len(keyList)):
+            key = re.findall(r'>.*</dt>', keyList[i])[0][1:-5]
+            # emmmmmmmmm什么鬼,,,,,,好吧,就是取 .*
+            try:
+                val = re.findall(r'\d+\.?.*</dd>, valueList[i]')[0][0:-5]
+                # 数字 点
+            except:
+                val = '--'
+            infoDict[key] = val
+
+        # 解释下下面这个麻瓜代码，如图
+        ''' <a class="bets-name" href="/stock/sz002415.html">
+                  海康威视 (<span>002415</span>)'''
+        infoDict.update(
+            {'股票名称':re.findall('\s.*\(',name)[0].spilt()[0] + \
+             re.findall('\>.*\<',name)[0][1:-1]})
+        yield infoDict
+        # infoDict = {name : 代号}
+
